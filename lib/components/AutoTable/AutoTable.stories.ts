77791e1f50ca62dc1fd1c7a1c26262788115f54a
Lee Winter <lee.winter@sainsburys.co.uk>
2024-04-01 15:19:30 +0100

    Fix circular storybook import ref

--- lib/components/AutoTable/AutoTable.stories.ts
+++ lib/components/AutoTable/AutoTable.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { AutoTable } from "@lib/components/AutoTable";
+import { AutoTable } from "@lib/components/AutoTable/AutoTable";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
